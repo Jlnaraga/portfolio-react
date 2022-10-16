d02b562c07a3f9486c02914f3cc607eb6431f520
Jovy Ira <ira_bonching@yahoo.com>
2022-10-15 22:04:00 -0400

    added react app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="about">About me</Link>
+          </li>
+          <li>
+            <Link to="portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="resume">Resume</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="main">
+        {/* Define all the routes */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="Portfolio" element={<Portfolio />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route path="resume" element={<Resume />}></Route>
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
 
+export const Home = () => {
+  return <div>You are in Home page</div>
+}
+export const About = () => {
+  return <div>This is the page where you put details about yourself</div>
+}
+export const NotFound = () => {
+  return <div>This is a 404 page</div>
+}
+    
 export default App;

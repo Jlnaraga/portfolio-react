79c366b3ecc027791d5519a118bdb90fe97ab736
Jovy Ira <ira_bonching@yahoo.com>
2022-10-16 21:48:14 -0400

    added html

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id="contact">
+        
+    <h1 class="headings">CONTACT ME</h1>
+    
+    <div class="container">
+    
+        <form>
+
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Your name.." />
+        
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Your email.." />
+        
+            <label for="message">Message</label>
+            <textarea id="message" name="message" placeholder="Write something.." rows="10"></textarea>
+        
+            <input type="submit" value="Submit" />
+
+        </form>
+    
+    </div>
+    
+</section>  
+    
   )
 }
 
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+            <h1 class="headings">PORTFOLIO</h1>
+            <div class="gallery">
+                <a href="https://kayhancock.github.io/Wanderlist/"><img src="./screenshots/wanderlist project 1.png" alt="" /></a>
+                <a href="" alt="https://github.com/mmelan000/Blue-Barramundi.git"><img src="./screenshots/find_a_friend.png" alt="" /></a>
+                <img src="./assets/images/pic3.jpg" alt="" />
+                <img src="./assets/images/pic4.jpg" alt="" />
+                <img src="./assets/images/pic5.jpg" alt="" />
+                <img src="./assets/images/pic6.jpg" alt="" />
+            </div>
+        </section>
+    
   )
 }
 

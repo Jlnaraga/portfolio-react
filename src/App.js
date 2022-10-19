70461d941aeed46cfa4f010f186dca950bd5d5d0
Jovy Ira <ira_bonching@yahoo.com>
2022-10-19 16:58:56 -0400

    added linkedin and github link

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Portfolio from './components/Portfolio/Portfolio';
 import Resume from './components/Resume/Resume';
 import './App.css';
-import React from "react"
-import { Routes, Route, NavLink as Link } from "react-router-dom"
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" className={({ isActive }) => isActive? "active": ''} end>Home</Link>
-          </li>
-          <li>
-            <Link to="about" className={({ isActive }) => isActive? "active": ''}>About</Link>
-          </li>
-          <li>
-            <Link to="portfolio" className={({ isActive }) => isActive? "active": ''}>Portfolio</Link>
-          </li>
-          <li>
-            <Link to="contact" className={({ isActive }) => isActive? "active": ''}>Contact</Link>
-          </li>
-          <li>
-            <Link to="resume" className={({ isActive }) => isActive? "active": ''}>Resume</Link>
-          </li>
-        </ul>
-      </nav>
+        <Navbar/>
       <div className="main">
         {/* Define all the routes */}
         <Routes>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+
+import linkedIn from "../../assets/images/svg/linkedin-icon.svg"
+import github from "../../assets/images/svg/github-icon.svg"
+
 import "./Home.css"
 
 const Home = () => {
@@ -11,6 +15,18 @@ const Home = () => {
                 <button className="btn">
                     Hire me
                 </button>
+            </div>
+
+            <div className="home_social">
+                <a href="https://www.linkedin.com/in/jovy-ira-naraga-9b1534238/" className="home_social-icon">
+                    <img src={linkedIn} alt=""/>
+                </a>
+                {/* <a href="https://codepen.io/" className="home_social-icon">
+                    <img src={codepen} alt=""/>
+                </a> */}
+                 <a href="https://github.com/Jlnaraga" className="home_social-icon">
+                    <img src={github} alt=""/>
+                </a> 
             </div>
         </section>
     )

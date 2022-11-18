734ca9cc2292330f655eaa9e2468e0974485a4a2
Jovy Ira <ira_bonching@yahoo.com>
2022-11-18 15:59:14 -0500

    about info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,10 +11,12 @@ return (
         <div id="intro">
             <h2>JOVY IRA NARAGA</h2>
             <br />
-            <p>I am a mom who studied to become a software developer.
-            I have two kids and 4 pet birds.   
-
-            
+            <p>Full-Stack Web Developer leveraging a background in the retail industry leading to resilience, 
+              the ability to connect easily with others, an unwavering calm demeanor in stressful situations, 
+              dedication and loyalty, and a diligent work ethic. </p>
+             
+              <p> Experienced in developing applications and communicating with other team members about 
+              decision making, time management, and problem solving together to ensure product delivery.
             </p>
         </div>
     </div>
